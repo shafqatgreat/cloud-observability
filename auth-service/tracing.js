7596a57efb58ff31987f2180fa5b86c573819596
Shafqat Altaf <shafqat@yahoo.com>
2026-01-17 19:07:29 +0500

    changes in tracing

--- auth-service/tracing.js
+++ auth-service/tracing.js
@@ -2,15 +2,10 @@
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
-const { Resource } = require("@opentelemetry/resources");
 
 const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
 const OTEL_EXPORTER_OTLP_HEADERS = process.env.OTEL_EXPORTER_OTLP_HEADERS; // should be like "Basic <token>"
 
-const resource = new Resource({
-  "service.name": "auth-service",
-  "service.version": "1.0.0",
-});
 
 const exporter = new OTLPTraceExporter({
   url: OTEL_EXPORTER_OTLP_ENDPOINT,
@@ -20,7 +15,7 @@ const exporter = new OTLPTraceExporter({
 });
 
 const sdk = new NodeSDK({
-  resource,
+  
   traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
