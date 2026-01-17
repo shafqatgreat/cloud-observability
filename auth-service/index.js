7f28662c08dee71250820677f3d906ec9c41b3eb
Shafqat Altaf <shafqat@yahoo.com>
2026-01-17 18:52:49 +0500

    changes in tracing

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,27 +1,23 @@
 require("./tracing");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
-app.post("/login",(req,res)=>{
-    const { username } = req.body;
-    // Simulate authentication
-    setTimeout(() => {
-        res.json({
-            status: "ok",
-            user: username,
-            token: "jwt-token-123"
-        });
-    },100);
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  // Simulate authentication
+  setTimeout(() => {
+    res.json({
+      status: "ok",
+      user: username,
+      token: "jwt-token-123",
+    });
+  }, 100);
 });
 
-// Adjust code for Railway cloud
-
+// Railway provides the port via environment variable
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Auth Service running on port ${PORT}`);
 });
-
-// app.listen(3000, () => {
-//   console.log("Auth Service running on port 3000");
-// });
