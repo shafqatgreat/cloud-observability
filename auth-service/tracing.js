4b9fbc921d38e56337ff31be1b36dee30ad592d8
Shafqat Altaf <shafqat@yahoo.com>
2026-01-17 18:42:46 +0500

    changes in tracing

--- auth-service/tracing.js
+++ auth-service/tracing.js
@@ -2,13 +2,28 @@
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const { resourceFromAttributes } = require("@opentelemetry/resources");
+const {
+  SERVICE_NAME,
+  SERVICE_VERSION,
+} = require("@opentelemetry/semantic-conventions");
 
+const resources = resourceFromAttributes({
+  [SERVICE_NAME]: "auth-service",
+  [SERVICE_VERSION]: "1.0.0",
+});
 
 
 const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
 const OTEL_EXPORTER_OTLP_HEADERS = process.env.OTEL_API_TOKEN;
 
-
+// Convert "Authorization=Basic xxx" → object
+// const headers = Object.fromEntries(
+//   OTEL_EXPORTER_OTLP_HEADERS.split(",").map(h => {
+//     const [k, v] = h.split("=");
+//     return [k, v];
+//   })
+// );
 const exporter = new OTLPTraceExporter({
     // url: "http://localhost:4318/v1/traces"
     // url: "https://shafqatgreat.grafana.net/otlp/v1/traces",
@@ -18,6 +33,7 @@ const exporter = new OTLPTraceExporter({
 
 
 const sdk = new NodeSDK({
+  resource: resources,
   traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
@@ -25,7 +41,6 @@ const sdk = new NodeSDK({
 sdk.start();
 
 console.log("Auth Service tracing initialized");
-
 
 
 
