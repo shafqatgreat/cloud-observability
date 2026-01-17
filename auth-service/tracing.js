7e244e11ea1c57345471c64c79a23cb29e97fd61
Shafqat Altaf <shafqat@yahoo.com>
2026-01-17 20:00:55 +0500

    changes in code to avoid 502 error

--- auth-service/tracing.js
+++ auth-service/tracing.js
@@ -10,7 +10,7 @@ const OTEL_EXPORTER_OTLP_HEADERS = process.env.OTEL_EXPORTER_OTLP_HEADERS; // sh
 const exporter = new OTLPTraceExporter({
   url: OTEL_EXPORTER_OTLP_ENDPOINT,
   headers: {
-    Authorization:"Basic%20MTQ5NzY5ODpnbGNfZXlKdklqb2lNVFkwTXpNek15SXNJbTRpT2lKdGVTMXVaWGN0ZEc5clpXNGlMQ0pySWpvaU1tWXdZWE13TkVwek5XazNNVlV6U0RWRk1uaElSRVpOSWl3aWJTSTZleUp5SWpvaWNISnZaQzF0WlMxalpXNTBjbUZzTFRFaWZYMD0=",
+    Authorization:`${OTEL_EXPORTER_OTLP_HEADERS}`,
   },
 });
 
