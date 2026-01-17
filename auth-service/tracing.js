62ef76743845a90e2a216e2583019ba65485e58d
Shafqat Altaf <shafqat@yahoo.com>
2026-01-17 20:11:30 +0500

    changes in code to avoid 502 error

--- auth-service/tracing.js
+++ auth-service/tracing.js
@@ -2,6 +2,12 @@
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const { SERVICE_NAME, SERVICE_VERSION } = require("@opentelemetry/semantic-conventions");
+
+const resource = new Resource({
+  [SERVICE_NAME]: "auth-service",
+  [SERVICE_VERSION]: "1.0.0",
+});
 
 const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
 const OTEL_EXPORTER_OTLP_HEADERS = process.env.OTEL_EXPORTER_OTLP_HEADERS; // should be like "Basic <token>"
@@ -15,7 +21,7 @@ const exporter = new OTLPTraceExporter({
 });
 
 const sdk = new NodeSDK({
-  
+  resource:resource,
   traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
