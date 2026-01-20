b72740e4c68f96191696abe105e56acdbbe53bee
Shafqat Altaf <shafqat@yahoo.com>
2026-01-20 16:41:17 +0500

    order service endpoin both order and orders

--- order-service/index.js
+++ order-service/index.js
@@ -5,6 +5,25 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/orders", (req, res) => {
+  res.json({
+    service: "order-service",
+    message: "Orders fetched successfully",
+
+    // identity propagated from gateway
+    user: {
+      id: req.headers["x-user-id"],
+      email: req.headers["x-user-email"],
+      role: req.headers["x-user-role"],
+    },
+
+    orders: [
+      { id: 1, item: "Laptop", price: 1200 },
+      { id: 2, item: "Phone", price: 800 },
+    ],
+  });
+});
+
 app.post("/order", (req, res) => {
   const userId = req.headers["x-user-id"];
     
