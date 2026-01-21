3032ae5b211ee62d5a84408f97413fde07f0defe
Shafqat Altaf <shafqat@yahoo.com>
2026-01-21 13:01:59 +0500

    Reverted

--- api-gateway-railway/tracing.js
+++ api-gateway-railway/tracing.js
@@ -62,3 +62,36 @@ module.exports = startTracing;
 
 
 
+
+
+// 'use strict';
+// const { NodeSDK } = require("@opentelemetry/sdk-node");
+// const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+// const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+// // const { Resource } = require("@opentelemetry/resources");
+
+// // const resource = new Resource({
+// //   "service.name": "auth-service",
+// //   "service.version": "1.0.0",
+// // });
+
+// const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+// const OTEL_EXPORTER_OTLP_HEADERS = process.env.OTEL_EXPORTER_OTLP_HEADERS; // should be like "Basic <token>"
+
+
+// const exporter = new OTLPTraceExporter({
+//   url: OTEL_EXPORTER_OTLP_ENDPOINT,
+//   headers: {
+//     Authorization:`${OTEL_EXPORTER_OTLP_HEADERS}`,
+//   },
+// });
+
+// const sdk = new NodeSDK({
+//   traceExporter: exporter,
+//   instrumentations: [getNodeAutoInstrumentations()],
+//   // resource,
+// });
+
+// sdk.start();
+
+// console.log("Auth Service tracing initialized");
